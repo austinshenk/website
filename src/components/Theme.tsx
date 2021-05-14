b654b858feb3156ee9988242c9daedeb26893b0e
Austin Bookhart <austin.bookhart@hannonhill.com>
2021-05-14 09:15:09 -0400

    Add basic ThemeProvider

---
+++ src/components/Theme.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export interface Theme {
+    spacing: (amount: number) => String
+}
+
+const ThemeContext = React.createContext<Theme>({
+    spacing: () => ""
+});
+
+type Props = React.PropsWithChildren<{
+    theme: Theme
+}>;
+
+export const ThemeProvider = (props: Props) => {
+    return <ThemeContext.Provider value={props.theme}>
+        {props.children}
+    </ThemeContext.Provider>
+};
+
+const useTheme = () => React.useContext(ThemeContext);
+export default useTheme;
